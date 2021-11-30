387150fd382f8645df0b3d1d9c6956bba27a8558
Allllakis <allakiselova20@gmail.com>
2021-11-30 17:04:06 +0300

    add posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,10 +3,38 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
-return <h4>posts page</h4>
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+  return <Layout>
+      <Hero />
+      <Posts posts={posts} title="All Posts"/>
+  </Layout>
 }
 
-
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM, Do YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default PostsPage
